d47db2726315e2e4d90ce6fd126f1479a6684985
Asma Robbana <nouiri.asma>
2025-03-05 21:23:41 -0500

    init_install:Tailwind+DaisyUi5/[Asma]/002

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
+    <button class="btn btn-primary">Button</button>
   </main>
 </template>
