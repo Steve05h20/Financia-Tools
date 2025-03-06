6d1171a17f756e55e7fbc5950ae192bc586e500b
Asma Robbana <nouiri.asma>
2025-03-05 21:57:24 -0500

    Ajout_dossier_modeles/[Asma]/003

---
+++ src/models/schoolDetails.interface.ts
@@ -0,0 +1,8 @@
+export interface ISchoolDetails {
+  id?: number
+  schoolName: string
+  fieldOfStudy: string
+  startDate: Date
+  projectedEndDate?: Date
+  // user:User à ajouter une fois interface user cree
+}
