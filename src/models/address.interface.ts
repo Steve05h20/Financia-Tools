a87b764d7e561bd952d9a7a9203b131f7a2ca7ec
Cristian Nino <cristiannino25@gmail.com>
2025-03-07 13:10:33 -0500

    creation interface address[cristian]002

--- src/models/address.interface.ts
+++ src/models/address.interface.ts
@@ -1,4 +1,26 @@
-export interface IAddress
-{
-afaire : "cristian"
+import type { IUser } from "./user.interface"
+
+
+enum EProvince {
+  "QC",
+  //  ici on doit metre les autres provinces ??
+}
+
+enum ECountry {
+  "CA",
+
+  //  ici on doit metre les autres pays ??
+
+
+}
+
+export interface IAddress {
+  id?: number
+  type: string
+  streetNumber: string
+  streetName: string
+  city: string
+  province: EProvince
+  country: ECountry
+  user: IUser
 }
