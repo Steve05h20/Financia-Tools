c167b6c6f2febddbff4d1cce55412773b2d985ec
Anoukbarnes <anouk721@gmail.com>
2025-03-08 16:51:37 -0500

    Ajout_Services_Get_User/[Anouk]/004

---
+++ src/services/useFetchUser.ts
@@ -0,0 +1,37 @@
+import { ref } from 'vue'
+import type { IUser } from "@/models/user.interface"
+
+export default function useFetchUser() {
+  const user = ref<IUser | null>(null)
+  const loading = ref<boolean>(false)
+  const error = ref<any>(null)
+
+  const GET_USER_BY_ID = async (userId: number): Promise<void> => {
+    loading.value = true
+    error.value = null
+
+    try {
+      const response = await fetch(`https://money-pie-3.fly.dev/api/v1/users/${userId}`)
+
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP: ${response.status}`)
+      }
+      const data = await response.json()
+      console.log('Données reçues:', data)
+      user.value = data
+      }
+     catch (err) {
+      error.value = err
+      console.error('Erreur lors de la récupération de l\'utilisateur:', err)
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return {
+    user,
+    loading,
+    error,
+    GET_USER_BY_ID
+  }
+}
