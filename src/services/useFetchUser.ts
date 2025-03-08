dc10d533dac792478724baf1058d45ff9a2667fe
Anoukbarnes <anouk721@gmail.com>
2025-03-08 17:41:18 -0500

    Ajout_services_put_user/[Anouk]/004

--- src/services/useFetchUser.ts
+++ src/services/useFetchUser.ts
@@ -56,6 +56,34 @@ export default function useFetchUser() {
       }
     }
 
+    const PUT_USER = async (userId: number, userData: IUser): Promise<void> => {
+      loading.value = true
+      error.value = null
+
+      try {
+        const response = await fetch(`https://money-pie-3.fly.dev/api/v1/users/${userId}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(userData)
+        })
+
+        if (!response.ok) {
+          throw new Error(`Erreur HTTP: ${response.status}`)
+        }
+
+        const data = await response.json()
+        console.log('Données reçues après création:', data)
+        user.value = data
+       } catch(err){
+          error.value = err
+          console.error('Erreur lors de la récupération de l\'utilisateur:', err)
+        } finally {
+          loading.value = false
+        }
+      }
+
 
     const DELETE_USER = async (userId: number): Promise<void> => {
       loading.value = true
@@ -91,6 +119,7 @@ export default function useFetchUser() {
     error,
     GET_USER_BY_ID,
     POST_USER,
+    PUT_USER,
     DELETE_USER,
   }
 }
