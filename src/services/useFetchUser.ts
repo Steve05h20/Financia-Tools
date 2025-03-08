41d297bee49ab445421b16a24e0385bf59ab9245
Anoukbarnes <anouk721@gmail.com>
2025-03-08 17:24:02 -0500

    Ajout_Services_Post_User/[Anouk]/004

--- src/services/useFetchUser.ts
+++ src/services/useFetchUser.ts
@@ -28,10 +28,41 @@ export default function useFetchUser() {
     }
   }
 
+  const POST_USER = async (newUser: IUser): Promise<void> => {
+    loading.value = true
+    error.value = null
+
+    try {
+      const response = await fetch('https://money-pie-3.fly.dev/api/v1/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+      })
+
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP: ${response.status}`)
+      }
+
+      const data = await response.json()
+      console.log('Données reçues après création:', data)
+      user.value = data
+     } catch(err){
+        error.value = err
+        console.error('Erreur lors de la récupération de l\'utilisateur:', err)
+      } finally {
+        loading.value = false
+      }
+    }
+
+
   return {
     user,
     loading,
     error,
-    GET_USER_BY_ID
+    GET_USER_BY_ID,
+    POST_USER
   }
 }
+
