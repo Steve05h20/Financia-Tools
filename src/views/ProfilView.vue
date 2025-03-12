4e72fa73dc5a6e9548786ca44083656a764d8e3c
Anoukbarnes <anouk721@gmail.com>
2025-03-12 11:34:40 -0400

    Déplacement detailsProfil dans profil existant

--- src/views/ProfilView.vue
+++ src/views/ProfilView.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import PersonnelInfo from '@/components/PersonnelInfo.vue'
+import AdressesInfo from '@/components/AdressesInfo.vue'
+import ScolaireInfo from '@/components/ScolaireInfo.vue'
+import BancairesInfo from '@/components/BancairesInfo.vue'
+</script>
 
 <template>
-  <main>
-    <h1>Profile</h1>
-  </main>
+
+  <section>
+   <PersonnelInfo />
+  </section>
+
+  <section>
+    <AdressesInfo />
+  </section>
+
+  <section>
+   <ScolaireInfo />
+  </section>
+
+  <section>
+   <BancairesInfo />
+  </section>
+
 </template>
+
+<style>
+</style>
+
