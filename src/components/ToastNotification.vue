1219a9a45d6afa396390aed877b275044673a9f9
Steve05h20 <144957548+Steve05h20@users.noreply.github.com>
2025-03-12 00:34:17 -0400

    Service de Notification/Steve/DEBUG  probleme de reactivité  apres merge

--- src/components/ToastNotification.vue
+++ src/components/ToastNotification.vue
@@ -1,21 +1,16 @@
 <script setup lang="ts">
-
 import useNotification from '@/services/useNotification';
 
-const useNotificationService = useNotification()
-const notifications = useNotificationService.notifications
+const { notifications } = useNotification();
 </script>
 
 <template>
 <div class="toast toast-top mt-24 toast-end w-48 font-semibold">
   <div
     v-for="notification in notifications"
-    :key="notification.message"
-    class="alert transition-all duration-500 flex items-center gap-2"
-    :class="[
-      `alert-${notification.type}`,
-      { 'translate-x-0': notification.isVisible, 'translate-x-52': !notification.isVisible }
-    ]"
+    :key="notification.id"
+    class="alert  transition-all duration-500 flex items-center gap-2"
+    :class="{ 'translate-x-0': notification.isVisible, 'translate-x-52': !notification.isVisible , 'alert-success': notification.type === 'success', 'alert-error': notification.type === 'error', 'alert-warning': notification.type === 'warning', 'alert-info': notification.type === 'info' }"
   >
     <span class="material-symbols-outlined text-white fill-white">
       {{ notification.type === 'success' ? 'check_circle' : notification.type === 'error' ? 'error' : notification.type === 'warning' ? 'warning' : 'info' }}
