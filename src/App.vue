eeafd74e15f610c7d82d3f50b35d72c080c089d3
Steve05h20 <144957548+Steve05h20@users.noreply.github.com>
2025-03-12 22:21:06 -0400

    preparation porut merge le serviceBankingDetails

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { useFetchBankingDetails } from './services/useFetchBankingDetails';
-
-const useBankingDetailsService = useFetchBankingDetails();
-
-useBankingDetailsService.getBankingDetailsByUserId(1);
-
 
 
 </script>
 
 <template>
-  <h2 v-if="useBankingDetailsService.loading.value">...</h2>
-  <h1 v-else>{{ useBankingDetailsService.error.value }}</h1>
-  <a class="link m-2" href="/profile">profil </a>
-  <a class="link m-2" href="/budget">budget </a>
-  <a class="link m-2" href="/">Home d</a>
-  <RouterView />
 
-
+  <RouterView/>
 
 </template>
