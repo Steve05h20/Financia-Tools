d8a5d447c5d78d54d639552d166fc512df0cc938
Anoukbarnes <anouk721@gmail.com>
2025-03-13 15:43:17 -0400

    Ajout_composant_label/[Anouk]/001

---
+++ src/components/Label.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { defineProps, withDefaults } from 'vue';
+
+interface LabelProps {
+  text: string;
+  htmlFor?: string;
+  required?: boolean;
+}
+
+withDefaults(defineProps<LabelProps>(), {
+  htmlFor: '',
+  required: false,
+});
+
+</script>
+
+<template>
+  <label :for="htmlFor" class="label w-full">
+    {{ text }}
+    <span v-if="required" >*</span>
+  </label>
+</template>
+
+<style>
+</style>
