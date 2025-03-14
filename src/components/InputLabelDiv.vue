c4cf20f748e58c9cf9d0fc1b39cddd0ffc7c3042
Anoukbarnes <anouk721@gmail.com>
2025-03-14 18:25:17 -0400

    Init_composant_section_Input-label/[Anouk]/001

---
+++ src/components/InputLabelDiv.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import Input from './Input.vue';
+import Label from './Label.vue';
+import { defineProps, withDefaults } from 'vue';
+
+interface InputLabelDivProps {
+  labelText: string;
+  htmlFor: string;
+  required?: boolean;
+  modelValue: string | number | Date | undefined;
+}
+
+withDefaults(defineProps<InputLabelDivProps>(), {
+  required: false,
+});
+
+const emit = defineEmits(['update:modelValue']);
+</script>
+
+<template>
+  <div>
+    <Label
+      :text="labelText"
+      :htmlFor="htmlFor"
+      :required="required"
+    />
+    <Input
+      :id="htmlFor"
+      :modelValue="modelValue"
+      @update:modelValue="value => emit('update:modelValue', value)"
+      v-bind="$attrs"
+    />
+  </div>
+</template>
