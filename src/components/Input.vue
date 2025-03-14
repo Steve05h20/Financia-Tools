d01e017fc1509ad9df5c93d3ad803377ec5d4d48
Anoukbarnes <anouk721@gmail.com>
2025-03-14 18:00:01 -0400

    ajout_modelValue_v-model_input/[Anouk]/001

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -4,18 +4,35 @@ import { defineProps, withDefaults } from 'vue';
 interface Props {
   type: 'text' | 'email' | 'date' | 'tel' | 'password' | 'number' | 'select';
   placeholder: string;
+  modelValue: string | number | Date | undefined;
+  id?: string;
 }
 
 withDefaults(defineProps<Props>(), {
   type: 'text',
+  placeholder: '',
+  id: '',
 });
+
+const emit = defineEmits(['update:modelValue']);
+
+const updateValue = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  emit('update:modelValue', target.value);
+};
 </script>
 
 
 <template>
 
   <div>
-    <input :type="type" :placeholder="placeholder" class="input input-ghost my-1" />
+    <input
+      :type="type"
+      :placeholder="placeholder"
+      :value="modelValue"
+      :id="id"
+      @input="updateValue"
+      class="input input-ghost my-1" />
   </div>
 
 </template>
