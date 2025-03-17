2f7bad0973789faaa51b309377b0c02ad40e9e92
Steve05h20 <144957548+Steve05h20@users.noreply.github.com>
2025-03-16 22:50:14 -0400

    integration du delete et updateTransaction du budgetStore/[Steve]/002

--- src/stores/useBudgetStore.ts
+++ src/stores/useBudgetStore.ts
@@ -17,7 +17,12 @@ export const useBudgetStore = defineStore('budget', () => {
   }
 
   const getTotalByType = (type: EType) => {
-    return transactions.value?.filter(transaction => transaction.type === type).reduce((acc, transaction) => acc + transaction.amount, 0);
+    if (!transactions.value || !Array.isArray(transactions.value)) {
+      return 0;
+    }
+    return transactions.value
+      .filter(transaction => transaction.type === type)
+      .reduce((acc, transaction) => acc + (Number(transaction.amount) || 0), 0);
   }
 
 
@@ -26,6 +31,23 @@ export const useBudgetStore = defineStore('budget', () => {
       throw new Error("ID utilisateur non valide");
     }
     await transactionService.CREATE_TRANSACTION_BY_USER_ID(userStore.user.id, {...transaction, type});
+    await loadTransactions();
+  }
+
+  const deleteTransaction = async (idTransaction: number) => {
+    if (typeof userStore.user.id !== 'number') {
+      throw new Error("ID utilisateur non valide");
+    }
+    await transactionService.DELETE_TRANSACTION_BY_USER_ID_AND_TRANSACTION_ID(userStore.user.id, idTransaction);
+    await loadTransactions();
+  }
+
+  const updateTransaction = async (idTransaction: number, transaction: ITransaction) => {
+    if (typeof userStore.user.id !== 'number') {
+      throw new Error("ID utilisateur non valide");
+    }
+    await transactionService.UPDATE_TRANSACTION_BY_USER_ID_AND_TRANSACTION_ID(userStore.user.id, idTransaction, transaction);
+    await loadTransactions();
   }
 
 
@@ -40,7 +62,9 @@ export const useBudgetStore = defineStore('budget', () => {
     transactionService,
     loadTransactions,
     getTotalByType,
-    addTransactionByType
+    addTransactionByType,
+    deleteTransaction,
+    updateTransaction
   }
 
 })
