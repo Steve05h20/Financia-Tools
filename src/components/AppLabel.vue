a258bb8eb6f930099eb61115f124d065a8df428f
Anoukbarnes <anouk721@gmail.com>
2025-03-17 19:22:47 -0400

    retrait_icon_modifier/[Anouk]/002

--- src/components/AppLabel.vue
+++ src/components/AppLabel.vue
@@ -1,8 +1,5 @@
 <script lang="ts" setup>
-import { useRoute, useRouter } from 'vue-router'
-import { computed } from 'vue';
 import { defineProps, withDefaults } from 'vue';
-import IconUpdate from '@/components/Profil/IconUpdate.vue';
 
 interface LabelProps {
   text: string;
@@ -15,20 +12,11 @@ withDefaults(defineProps<LabelProps>(), {
   htmlFor: '',
   required: false,
 });
-
-const route = useRoute();
-const isProfil = computed(() => {
-  return route.path.includes('/profile') || route.name === 'profile';
-});
-
 </script>
 
 <template>
   <label :for="htmlFor" class="label w-full text-info relative group">
     {{ text }}
     <span v-if="required" class="text-error" >*</span>
-    <div v-if="isProfil" class="absolute right-0 top-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-      <IconUpdate />
-    </div>
   </label>
 </template>
