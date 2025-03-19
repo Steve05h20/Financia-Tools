97864b59faa283f9c2c9011e4b49acae7995bddf
Anoukbarnes <anouk721@gmail.com>
2025-03-19 11:15:58 -0400

    Ajout_btn_modifier_store/[Anouk]/002

---
+++ src/stores/profil/useEditStore.ts
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia";
+import { ref } from 'vue';
+
+export const useEditStore = defineStore('edit', () => {
+  const isEditing = ref<boolean>(false);
+
+  const toggleEditing = (): void => {
+    isEditing.value = !isEditing.value;
+  };
+
+  const saveChanges = (): void => {
+   //Ajouter logique PUT
+    console.log('Sauvegarde des données...');
+    isEditing.value = false;
+  };
+
+  return {
+      isEditing,
+      toggleEditing,
+      saveChanges
+      };
+});
