4db6a740771b27619b27c5769a2a313ff6908a83
Steve05h20 <144957548+Steve05h20@users.noreply.github.com>
2025-03-31 09:24:52 -0400

    ajout image mobile/[steve]/003

--- src/components/Home/Exemple/dataSection.ts
+++ src/components/Home/Exemple/dataSection.ts
@@ -8,7 +8,7 @@ export const dataSection = ()=>{
   }
 
   const images = {
-    imageHome: {url: "https://images.unsplash.com/photo-1741800465167-0c276ad75e08?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", alt: "personIce"},
+    imageHome: {url: "src/components/Home/Exemple/phone.png", alt: "personIce"},
   }
 
   return {
--- src/components/Home/Exemple/viewSection.vue
+++ src/components/Home/Exemple/viewSection.vue
@@ -11,12 +11,13 @@ const { texts, images, nameSection } = dataSection()
 <template>
   <SectionGrid :id="nameSection" colsDesktop="12" colsTablet="6" colsMobile="1"  >
 
-    <div class="col-span-10  my-auto">
-        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">{{ texts.title }}</h1>
+    <div class="col-span-9 my-auto">
+        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">{{ texts.title }}</h1>
         <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">{{ texts.description }}</p>
+        <button class="btn btn-primary btn-lg">Get Started</button>
     </div>
-    <div class="col-span-2 col-end-13 ">
-        <img  :src="images.imageHome.url" :alt="images.imageHome.alt" class="object-cover h-96 w-96 rounded-full"  />
+    <div class="col-span-3 col-end-13 ">
+        <img  :src="images.imageHome.url" :alt="images.imageHome.alt" class="object-contain w-[200px] max-sm:w-[150px]"  />
     </div>
 
   </SectionGrid>
