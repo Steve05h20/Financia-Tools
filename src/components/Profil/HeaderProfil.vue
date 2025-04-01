1751a548e579f92e97754e099299aaa46a9a4dc1
Anoukbarnes <anouk721@gmail.com>
2025-04-01 14:40:07 -0400

    ajustement_padding/[Anouk]/001

--- src/components/Profil/HeaderProfil.vue
+++ src/components/Profil/HeaderProfil.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="grid grid-cols-1 grid-cols-column lg:grid-cols-4 lg:my-8">
+  <div class="grid grid-cols-1 grid-cols-column lg:grid-cols-4 lg:my-8 px-4">
     <h1 class="text-4xl lg:col-span-1">Mon profil</h1>
   </div>
 
