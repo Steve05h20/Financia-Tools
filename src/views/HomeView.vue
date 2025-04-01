c9f0ad4f475a489cd2ef43dc0c1a1eb54101c9a0
Asma Robbana <nouiri.asma>
2025-04-01 01:10:12 -0400

    section conférence landing page/[Asma]/001

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
-import ViewSection from '@/components/Home/Exemple/viewSection.vue';
-
-
+import ViewConference from '@/components/Home/Conferences/viewConference.vue'
+import ViewSection from '@/components/Home/Exemple/viewSection.vue'
 </script>
 
 <template>
   <main>
     <!-- exemple -->
     <ViewSection />
-     <!-- exemple -->
-
-
-
+    <!-- exemple -->
+    <ViewConference />
   </main>
 </template>
