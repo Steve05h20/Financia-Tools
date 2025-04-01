0fb05c6f51193514b3ce2a3211ca82ba26e3bdff
Anoukbarnes <anouk721@gmail.com>
2025-04-01 14:40:24 -0400

    retait_commentaires/[Anouk]/001

--- src/services/useAddFormToggle .ts
+++ src/services/useAddFormToggle .ts
@@ -4,12 +4,9 @@ import { useEditStore } from '@/stores/profil/useEditStore'
 const useAddFormToggle = () => {
   const editStore = useEditStore()
 
-  // État détermine si le formulaire doit être affiché
   const showForm = ref<boolean>(false)
 
-  // Vérifie si les données existent et ne sont pas vides
   const checkDataExists = (data: any[] | undefined): void => {
-    // Affiche le formulaire si le tableau existe et n'est pas vide
     showForm.value = Boolean(data && data.length > 0)
   }
 
